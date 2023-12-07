a922fc2c712a6be32691f55e481c3955a3b0af92
keamybams <rulibams16@gmail.com>
2023-12-07 09:30:33 +0100

    guess

---
+++ Week 7/Guessing game/script.js
@@ -0,0 +1,18 @@
+function guessRandom() {
+    let num = parseInt(document.getElementById("input").value);
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    let result = document.getElementById("result");
+
+    console.log(randomNumber);
+
+    if (num < 1 || num > 100 || isNaN(num)) {
+        result.innerHTML = 'Please enter a valid number'
+    } else if (num > randomNumber) {
+        result.innerHTML = 'Too High';
+    } else if (num < randomNumber) {
+        result.innerHTML = 'Too Low';
+    } else {
+        result.innerHTML = 'Correct';
+    }
+}
+
